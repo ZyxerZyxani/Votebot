954b8e896dde8af6cd8f5cc6a2206a19ab0ad165
ZyxerZyxani <33463214+ZyxerZyxani@users.noreply.github.com>
2019-04-24 20:29:57 +0200

    Update VoteBotGithub.py

--- VoteBotGithub.py
+++ VoteBotGithub.py
@@ -7,6 +7,8 @@ import random
 
 client = Bot(description="VoteBot By Zyxer", command_prefix="!", pm_help = False)
 voters = []
+voterID = []
+voterid = ''
 g = 0
 x = 0
 y = 0
@@ -31,7 +33,7 @@ async def ja(ctx):
         voters.append(auth) #Adds the voters name to list, just to prevent someone from voting twice or more
         subject="votes{}".format(g)#      Change 'votes' to whatever you want the votefile to be called, for example {subject="MoreCheetos"}
         F = open(subject, 'a+') #defines 'F', now 'F' opens a textfile called votes, if such a textfile does not exist, it creates one
-        F.write("{} {}\n".format(vote, voterid)) #Adds the vote to the bottom of the textfile 'F' opens. Location should be where this script is kept.
+        F.write("Ja {}\n".format(voterid)) #Adds the vote to the bottom of the textfile 'F' opens. Location should be where this script is kept.
         F.close()
         msg = "Your vote has now been cast. Your votes ID is {}".format(voterid)
 #        async for msg in client.logs_from(ctx.message.channel):
@@ -41,18 +43,26 @@ async def ja(ctx):
         x = x+1
 @client.command(pass_context=True)
 async def nej(ctx):
-    auth = "{}".format(ctx.message.author.mention) #Saves the name of the one voting
+    auth = "{}".format(ctx.message.author.id) #Saves the name of the one voting
     if auth in voters: #Checks if the one voting has already voted
 #        async for msg in client.logs_from(ctx.message.channel):
 #            await client.delete_message(msg)
         msg = "Your vote has already been cast"
         await client.send_message(ctx.message.channel, msg) #Tells those that try to vote more than once that they have already voted
     else :
+        voterid = random.randint(0, 1000000)# generates a random number from 0-1000000 to use as an ID so the voter can check and confirm that his/her vote has been counted
+        while voterid in voterID:# Checks if the ID already exists as to not make any duplicate
+            voterid = random.randint(0, 1000000)# Generates another ID if there is another identical ID number
+        voterID.append(voterid)# Adds the ID to the list so it can later check if there is a duplicate
         voters.append(auth) #Adds the voters name to list, just to prevent someone from voting twice or more
-        msg = "Your vote has now been cast."
+        subject="votes{}".format(g)#      Change 'votes' to whatever you want the votefile to be called, for example {subject="MoreCheetos"}
+        F = open(subject, 'a+') #defines 'F', now 'F' opens a textfile called votes, if such a textfile does not exist, it creates one
+        F.write("Nej {}\n".format(voterid)) #Adds the vote to the bottom of the textfile 'F' opens. Location should be where this script is kept.
+        F.close()
+        msg = "Your vote has now been cast. Your votes ID is {}".format(voterid)
 #        async for msg in client.logs_from(ctx.message.channel):
 #            await client.delete_message(msg)
-        await client.send_message(ctx.message.channel, msg) #PMs the voter that their vote has been cast.
+        await client.send_message(ctx.message.author, msg) #PMs the voter that their vote has been cast.
         global y
         y = y+1
 
@@ -67,6 +77,17 @@ async def votes(ctx):
     y = 0
     g = g+1
     voters[:] = []
+    voterID[:] = []
+    
+#@client.command(pass_context=True)
+#async def sdeck(ctx):
+#    
+#@client.command(pass_context=True)
+#async def ldeck(ctx):
+#    
+#
+#@client.command(pass_context=True)
+#async def cdeck(ctx):
 ################################################################################################################
 #@client.command(pass_context=True)
 #async def vote1(ctx, x):#vote1 is what the bot listens for to apply the vote in this part, so '!vote' can be about one thing and '!vote1' can be about another thing
